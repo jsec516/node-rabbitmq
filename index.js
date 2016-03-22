0eb15dbe61e9b9613460fe795b8c0d8366a09b78
Jahidul Islam <jsec516@gmail.com>
2016-03-22 12:06:56 +0600

    working rabbitmq

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ var connection = amqp.createConnection({url: "amqp://guest:guest@localhost:5672"
 
 // Wait for connection to become established.
 connection.on('ready', function () {
-  // Use the default 'amq.topic' exchange
+ var exchange = connection.exchange(''); // get the default exchange  
+// Use the default 'amq.topic' exchange
   connection.queue('my-queue', function(q){
       // Catch all messages
       q.bind('#');
 
-      console.log('listening to messages');
+      // console.log('listening to messages');
 
       // Receive messages
       q.subscribe(function (message) {
         // Print messages to stdout
-        console.log(message);
+       // console.log('publish message');
+        console.log(message.body);
       });
+      // publish a message
+      exchange.publish(q.name, {body: 'Hello RabbitMQ!'}); 
   });
+  
 });
 
+//connection.end();
