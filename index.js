145b04d3756f7e8c2a53ef5f0d06e361c6731102
Jahidul Islam <jsec516@gmail.com>
2016-03-22 11:40:32 +0600

    listener added

---
+++ index.js
@@ -0,0 +1,20 @@
+var amqp = require('amqp');
+var connection = amqp.createConnection({url: "amqp://guest:guest@localhost:5672"});
+
+// Wait for connection to become established.
+connection.on('ready', function () {
+  // Use the default 'amq.topic' exchange
+  connection.queue('my-queue', function(q){
+      // Catch all messages
+      q.bind('#');
+
+      console.log('listening to messages');
+
+      // Receive messages
+      q.subscribe(function (message) {
+        // Print messages to stdout
+        console.log(message);
+      });
+  });
+});
+
